e1f5c4471a1354b337a203a408045fc17753b835
chengchengwang <cw29642tc.columbia.edu>
2017-04-04 12:11:34 -0400

    Start

--- app/src/main/java/chengcheng/leaguage/LogIn.java
+++ app/src/main/java/chengcheng/leaguage/LogIn.java
@@ -3,11 +3,11 @@ package chengcheng.leaguage;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LogIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_log_in);
     }
 }
