6cc23e204dfed75c3e2139eed09489773760f1dd
jingshanW <jw3435@tc.columbia.edu>
2017-04-24 15:34:31 -0400

    4.24 jingshan

--- app/src/main/java/chengcheng/leaguage/Welcome.java
+++ app/src/main/java/chengcheng/leaguage/Welcome.java
@@ -3,6 +3,7 @@ package chengcheng.leaguage;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class Welcome extends AppCompatActivity {
 
@@ -12,14 +13,23 @@ public class Welcome extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void Login() {
+
+//    public void Login() {
+
+//    }
+
+    public void userSignUp(View view) {
+        Intent intent = new Intent(Welcome.this, SignUp.class);
+        startActivity(intent);
+    }
+
+    public void userLogIn(View view) {
         Intent intent = new Intent(Welcome.this, LogIn.class);
         startActivity(intent);
     }
 
-    public void Signup() {
-        Intent intent = new Intent(Welcome.this, SignUp.class);
-        startActivity(intent);
-    }
+//    public void SignUp() {
+
+//    }
 
 }
